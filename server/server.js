8855a0590f395f12012fa69af60c95b006c7e944
Bryan A <easyrun32@gmail.com>
2022-04-15 20:16:37 -0400

    heroku

--- server/server.js
+++ server/server.js
@@ -1,44 +1,25 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const bodyParser = require('body-parser');
 const path = require('path');
-
-const { jwtStrategy } = require('./config/passport');
-const passport = require('passport');
-
-const routes = require('./routes/index');
-require('dotenv').config();
-
 const app = express();
-const port = process.env.PORT || 8000;
-// db
+const port = process.env.PORT || 5000;
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-mongoose
-  .connect(process.env.DATABASE)
-  .then(() => console.log('DB connected'))
-  .catch((err) => console.log('DB Error => ', err));
-
-// middlewares
-app.use(express.static('public'));
-app.use(express.json({ limit: '5mb' }));
-app.use(cors());
-// jwt authentication
-app.use(passport.initialize());
-passport.use('jwt', jwtStrategy);
-
+app.get('/api/hello', (req, res) => {
+  res.send({ express: 'Hello From Expressss' });
+});
+app.post('/api/world', (req, res) => {
+  res.send(
+    `I received your POST request. This is what you sent me: ${req.body.post}`
+  );
+});
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, 'client/build')));
 
   app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
 
-// autoload routes
-// readdirSync('./routes').map((r) => app.use('/api', require(`./routes/${r}`)));
-app.use(routes);
-
-// listen
-
-console.log(port);
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+app.listen(port, () => console.log(`Listening on port ${port}`));
